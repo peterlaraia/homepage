95de93c1aefeda872568d038e265bfe279e77642
Peter Laraia <PeterLaraiaGit@gmail.com>
2017-07-30 11:33:38 -0500

    project: grow header font on hover

--- src/app/projects/project.js
+++ src/app/projects/project.js
@@ -5,18 +5,20 @@ import './project.scss';
 export function Project(props) {
     const { project } = props;
     return (
-        <div className='project-section'>
-            <div className='project-header flex-center'>
-                <a href={project.link.main}><h4>{project.name}</h4></a>
-            </div>
-            <div className='project-content'>
-                <div className='project-notes flex-center'>
-                    NOTES
+        <a className='clean-link pulse' href={project.link.main}>
+            <div className='project-section'>
+                <div className='project-header flex-center pulse-font'>
+                    <h4>{project.name}</h4>
                 </div>
-                <div className='project-description flex-center'>
-                    {project.summary}
+                <div className='project-content'>
+                    <div className='project-notes flex-center'>
+                        NOTES
+                    </div>
+                    <div className='project-description flex-center'>
+                        {project.summary}
+                    </div>
                 </div>
             </div>
-        </div>
+        </a>
     );
 }
