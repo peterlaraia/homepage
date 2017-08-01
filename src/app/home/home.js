51242d69454f2db2bd64a740fa125b6b29d56e3d
Peter Laraia <PeterLaraiaGit@gmail.com>
2017-08-01 12:49:23 -0500

    scatter in static position

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -11,48 +11,30 @@ const style = {
     justifyContent: 'center'
 }
 
-const LOCATIONS = [
-    {top: '22%', left: '6%'},
-    {top: '79%', left: '7%'},
-    {top: '2%', left: '33%'},
-    {top: '29%', left: '28%'},
-    {top: '54%', left: '29%'},
-    {top: '8%', left: '57%'},
-    {top: '47%', left: '58%'},
-    {top: '3%', left: '80%'},
-    {top: '25%', left: '78%'},
-    {top: '78%', left: '73%'}
-
-]
-
 export class Home extends React.Component {
 
     state = {
-        images: []
+        pieces: []
     }
 
     componentDidMount() {
         ContentService.getMe().subscribe(me => {
             this.setState((state) => ({
-                ...state, images: me.map(piece => piece.img)
+                ...state, pieces: me
             }));
         });
     }
 
-    
-
     render() {
-        const { images } = this.state;
-        const locations = LOCATIONS.slice();
+        const { pieces } = this.state;
         return (
             <div className='container vertical' style={{height: '100%'}}>
                 <h3 style={style}>Hi!</h3>
                 <div className='scattered'>
                     {
-                        images.map(image => {
-                            const [location] = locations.splice(Math.floor(Math.random() * locations.length), 1);
+                        pieces.map(me => {
                             return (
-                                <Scatter key={image} image={image} location={location} />
+                                <Scatter key={me.id} image={me.img} location={me.location} />
                             );
                         })
                     }
